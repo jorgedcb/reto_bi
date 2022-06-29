bd1b0063a1e6208cbb41ed5c402d2198a5d7e5d9
jorgedcb <jecb2001@gmail.com>
2022-06-29 00:35:34 -0500

    everything done, more proves left to do

--- OOP/main.py
+++ OOP/main.py
@@ -1,38 +1,9 @@
-from wordle import Wordle
+from example import Wordle
 import json
 
 with open('/home/castilla/Desktop/sofka/reto_bi/OOP/test.txt', 'r') as f:
     list_words = json.loads(f.read())
 
-#user = ('jorge.castilla','5cbdaf7e3c844ec882f576ec2ec4c9a4')
-user = ('jorge','sofka')
+game = Wordle(list_words)
+game.play()
 
-
-
-game = Wordle(list_words,user)
-
-first_attempt = game.get_attempt()
-print(first_attempt)
-game.send_attemp(first_attempt)
-second_attemp = game.get_attempt()
-print(second_attemp)
-game.send_attemp1(second_attemp)
-third_attemp = game.get_attempt()
-print(third_attemp)
-game.send_attemp2(third_attemp)
-#print(game.list_results)
-
-#print(game.list_letter_index_incorrect)
-print(game.get_attempt())
-#game.send_attemp2(game.get_attempt())
-#game.send_attemp2(game.get_attempt())
-
-
-
-#print(game1.attempt_list)
-# print(game1.user)
-# print(game1.parameters)
-# print(game1.id)
-# print(game1.length_word)
-# print(game1.vowels)
-# print(game1.consonants)
--- OOP/wordle.py
+++ OOP/wordle.py
@@ -1,45 +1,59 @@
-from unittest import result
 import requests
 import pandas as pd
 import numpy as np
+import json
+from os import path
+
+
 class Wordle:
-    all = []
-    parameters_endpoint = 'https://7b8uflffq0.execute-api.us-east-1.amazonaws.com/game/get_params'
-    results_endpoint = 'https://7b8uflffq0.execute-api.us-east-1.amazonaws.com/game/check_results'
-    def __init__(self,words_list,user:tuple):
 
-        #add a check for the user
+    default_user = ("jorge.castilla", "5cbdaf7e3c844ec882f576ec2ec4c9a4")
+    parameters_endpoint = "https://7b8uflffq0.execute-api.us-east-1.amazonaws.com/game/get_params"
+    results_endpoint = "https://7b8uflffq0.execute-api.us-east-1.amazonaws.com/game/check_results"
+    filename = "/home/castilla/Desktop/sofka/reto_bi/OOP/responses.json"
 
+    def __init__(
+        self,
+        words_list,
+        user=default_user,
+        parameters_endpoint=parameters_endpoint,
+        results_endpoint=results_endpoint,
+        filename=filename,
+    ):
 
-        # Run validations to the received arguments
-        # assert price >= 0, f"Price {price} is not greater than or equal to zero!"
-        
         # Assign to self object
-        self.list_letter_index_incorrect = [] #check if this is the best way to do it
-        self.wrong_letters = [] #check if this is the best way to do it
-        self.__num_attempts  = 0
+
+        self.parameters_endpoint = parameters_endpoint
+        self.results_endpoint = results_endpoint
+        self.filename = filename
+        self.__num_attempts = 0
         self.__attempt_list = []
-        self.__last_attempt = ''
+        self.__last_attempt = ""
         self.__list_results = []
-        self.user = user
-        self.__parameters = self._get_parameters(user) # tengo que decirle .json cuando sea el de verdad
-        self.__id = self.__parameters['id']
-        self.__length_word = self.__parameters['length_word']
-        self.__vowels = self.__parameters['vowels']
-        self.__consonants = self.__parameters['consonants']
+        self.__user = user
+        parameters = self._get_parameters()
+        self.__parameters = parameters.json()
+        self.__id = self.__parameters["id"]
+        self.__length_word = self.__parameters["length_word"]
+        self.__vowels = self.__parameters["vowels"]
+        self.__consonants = self.__parameters["consonants"]
         self.__words_list = words_list
         self.__df_game = self._get_df_game()
+        self.__wrong_letters = []
+        self.__must_letters = []
+        self.__list_letter_index_incorrect = []
+
     @property
     def num_attempts(self):
-        return self.__num_attempts #add funtionality that can't find the word in 6 attempps
+        return self.__num_attempts
 
     def _increase_num_attempts(self):
-        self.__num_attempts += 1 
+        self.__num_attempts += 1
 
     @property
     def attempt_list(self):
         return self.__attempt_list
-    
+
     def _append_attempt(self):
         self.__attempt_list.append(self.last_attempt)
         print(f"{self.last_attempt} - attempt #{self.num_attempts}")
@@ -48,18 +62,26 @@ class Wordle:
     def last_attempt(self):
         return self.__last_attempt
 
-    def _set_last_attempt(self,word):
+    def _set_last_attempt(self, word):
         self.__last_attempt = word
-    
+
     @property
     def list_results(self):
         return self.__list_results
-    
-    def _append_result(self,json):
-        self.__list_results.append(json)
-        print(json) #add .json cuando sea el de verdad y arreglar como se ve
 
-    def _get_parameters(self,user):
+    @property
+    def user(self):
+        return self.__user
+
+    def _append_result(self, result):
+        self.__list_results.append(result)
+        print(result.json())
+
+    def print_results(self):
+        for response in self.list_results:
+            print(response.json())
+
+    def _get_parameters(self):
         """The format of the response is:
         {"id": "string",
         "length_word": int,
@@ -67,9 +89,11 @@ class Wordle:
         "consonants": int
         }
         """
-        just_prove = {'id': '62b4e50da15bde70e02b0f6e', 'length_word': 8, 'vowels': 4, 'consonants': 4}
-        return just_prove
-        #return requests.get(self.parameters_endpoint, auth = user)
+        response = requests.get(self.parameters_endpoint, auth=self.user)
+        self._append_response("nuevo juego")
+        self._append_response(response.json())
+        print(response.json())
+        return response
 
     @property
     def parameters(self):
@@ -82,11 +106,11 @@ class Wordle:
     @property
     def length_word(self):
         return self.__length_word
-    
+
     @property
     def vowels(self):
         return self.__vowels
-    
+
     @property
     def consonants(self):
         return self.__consonants
@@ -95,153 +119,251 @@ class Wordle:
     def words_list(self):
         return self.__words_list
 
-    
     @property
     def df_game(self):
         return self.__df_game
-    
+
+    @property
+    def must_letters(self):
+        return self.__must_letters
+
+    @property
+    def wrong_letters(self):
+        return self.__wrong_letters
+
     def _create_df_from_list(self):
-        self.df_all_words = pd.DataFrame(self.words_list, columns =['Words'])
+        self.df_all_words = pd.DataFrame(self.words_list, columns=["Words"])
 
     def _add_number_letters_column(self):
-        self.df_all_words['Number of letters'] = self.df_all_words['Words'].str.len()    
-    
+        self.df_all_words["Number of letters"] = self.df_all_words[
+            "Words"
+        ].str.len()
+
     def _add_vowels_column(self):
-        self.df_all_words['Number of vowels'] = self.df_all_words['Words'].str.count(r'[aeiou]')
+        self.df_all_words["Number of vowels"] = self.df_all_words[
+            "Words"
+        ].str.count(r"[aeiou]")
 
     def _add_consonant_column(self):
-        self.df_all_words['Number of consonants'] = (self.df_all_words['Number of letters'] 
-                                                     - self.df_all_words['Number of vowels'])
-                                            
+        self.df_all_words["Number of consonants"] = (
+            self.df_all_words["Number of letters"]
+            - self.df_all_words["Number of vowels"]
+        )
+
     def _restrict_df_by_parameter(self):
-        conditions = (self.df_all_words['Number of letters'] == self.__length_word)  & (self.df_all_words['Number of vowels'] == self.__vowels) & (self.df_all_words['Number of consonants'] == self.consonants)
+        conditions = (
+            (self.df_all_words["Number of letters"] == self.__length_word)
+            & (self.df_all_words["Number of vowels"] == self.__vowels)
+            & (self.df_all_words["Number of consonants"] == self.consonants)
+        )
         return self.df_all_words.loc[conditions]
 
     def _get_df_game(self):
+        """Return a Pandas Dataframe created from the list of possible words."""
         self._create_df_from_list()
         self._add_number_letters_column()
         self._add_vowels_column()
         self._add_consonant_column()
         return self._restrict_df_by_parameter()
 
-    def valid_words_list(self):
-        return list(self.df_game['Words'].values)
+    def _valid_words_list(self):
+        return list(self.df_game["Words"].values)
 
-    def word_from_list(self,list_words):
-        print(len(list_words))
+    def _word_from_list(self, list_words):
+        print(f"There are {len(list_words)} words left")
         new_attempt = max(list_words, key=lambda word: len(set(word)))
         return new_attempt
 
-
-    def first_attempt(self):
-        word = self.word_from_list(self.valid_words_list())
-        return word
-
-
-    def valid_words(self, list_tupple):
-        #add a comment
+    def _valid_words(self, index_correct_letters):
+        """Get words that match the letter and position which the api return True.
+        I.e: If i send jorge and the api return [False, True, False, False, False]
+        get the words with an 'o' in second position."""
+        list_letter_index_correct = [
+            (index, letter)
+            for index, letter in enumerate(self.last_attempt)
+            if index in index_correct_letters
+        ]
         df = self.df_game
-        if list_tupple:
-            bool_words = [df['Words'].str[index].eq(letter) for index, letter in list_tupple]
-            list_words = list(df.loc[np.logical_and.reduce(bool_words), 'Words'].values)
+        if list_letter_index_correct:
+            bool_words = [
+                df["Words"].str[index].eq(letter)
+                for index, letter in list_letter_index_correct
+            ]
+            list_words = list(
+                df.loc[np.logical_and.reduce(bool_words), "Words"].values
+            )
             return list_words
-        else: 
-            return self.valid_words_list()
-    
-    def invalid_words(self,list_tupple):
+        else:
+            return self._valid_words_list()
+
+    def _invalid_words(self, index_correct_letters):
+        incorrect_index_try = [
+            index
+            for index, letter in enumerate(self.last_attempt)
+            if (
+                letter in set(self.must_letters)
+                and index not in index_correct_letters
+            )
+        ]
+        self.__list_letter_index_incorrect.extend(
+            [
+                (index, letter)
+                for index, letter in enumerate(self.last_attempt)
+                if index in incorrect_index_try
+            ]
+        )
         df = self.df_game
-        if list_tupple:
-            bool_words = [df['Words'].str[index].eq(letter) for index, letter in list_tupple]
-            list_words = list(df.loc[np.logical_or.reduce(bool_words), 'Words'].values)
+        if self.__list_letter_index_incorrect:
+            bool_words = [
+                df["Words"].str[index].eq(letter)
+                for index, letter in self.__list_letter_index_incorrect
+            ]
+            list_words = list(
+                df.loc[np.logical_or.reduce(bool_words), "Words"].values
+            )
             return list_words
-        else: 
+        else:
             return []
 
-     
-
-    
-    def new_attempt(self):
-        #index where the list of bools were true
-        correct_index_letters_try = np.where(self.last_result_json['position_array'])[0]
-        
-        #list of letters that are in the correct position
-        letter_correct_position = [self.last_attempt[j] for j in correct_index_letters_try]
-        right_letters = self.last_result_json['right_letters_in_wrong_positions']
+    def _update_must_letters(self, index_correct_letters):
+        """Letters that must be in the word."""
+        letter_correct_position = [
+            self.last_attempt[j] for j in index_correct_letters
+        ]
+        right_letters = self.__last_result_json[
+            "right_letters_in_wrong_positions"
+        ]
         right_letters.extend(letter_correct_position)
-        
-        #list of letters that definitely aren't in the word
-        wrong_letters_last_try = list(set(self.last_attempt) - set(right_letters))
-        
-        #right_letters = list(set(right_letters))#quitar esto en un futuro
-        self.wrong_letters.extend(wrong_letters_last_try)
-        
-        #getting index from letters that are in the word but are in wrong position
-        #erase the index if are in the index of correct words
-        incorrect_index_try = index_letters_wrong_position = [index for index,letter in enumerate(self.last_attempt) if (letter in set(right_letters) and index not in correct_index_letters_try )] 
-        
-        #get the tuples that are right
-        list_letter_index_correct = [(index,letter) for index,letter in enumerate(self.last_attempt) if index in correct_index_letters_try]
-        #get tupples that the letter exits on the word but are in wrong position
-        self.list_letter_index_incorrect.extend([(index,letter) for index,letter in enumerate(self.last_attempt) if index in incorrect_index_try])
+        self.__must_letters = right_letters
+        return self.must_letters
 
-        valid_words = self.valid_words(list_letter_index_correct)
-        
-        invalid_words = self.invalid_words(self.list_letter_index_incorrect)
+    def _update_wrongs_letters(self):
+        """Letters that can't be in the word."""
+        wrong_letters_last_try = list(
+            set(self.last_attempt) - set(self.must_letters)
+        )
+        self.__wrong_letters.extend(wrong_letters_last_try)
 
-        valid_list = [word for word in valid_words if word not in invalid_words]
-        a_list = [word for word in valid_list if all(invalid_letter not in word for invalid_letter in self.wrong_letters)]
-        #valid letters mean must letters
-        final_list = [word for word in a_list if all(right_letters.count(valid_letter) <= word.count(valid_letter) for valid_letter in right_letters)]
+    def _final_list(self, valid_words, invalid_words):
+        """Return list of words that watch all the filters."""
+        right_letters = self.must_letters
+        valid_list = [
+            word for word in valid_words if word not in invalid_words
+        ]
+        a_list = [
+            word
+            for word in valid_list
+            if all(
+                invalid_letter not in word
+                for invalid_letter in self.wrong_letters
+            )
+        ]
+        final_list = [
+            word
+            for word in a_list
+            if all(
+                right_letters.count(valid_letter) <= word.count(valid_letter)
+                for valid_letter in right_letters
+            )
+        ]
+        return final_list
 
-        new_try = self.word_from_list(final_list)
+    def _first_attempt(self):
+        word = self._word_from_list(self._valid_words_list())
+        return word
+
+    def _new_attempt(self):
+        """Thit method is used to get any attempt after the first one."""
+        correct_index_letters_try = np.where(
+            self.__last_result_json["position_array"]
+        )[0]
+        self._update_must_letters(correct_index_letters_try)
+        self._update_wrongs_letters()
+        valid_words = self._valid_words(correct_index_letters_try)
+        invalid_words = self._invalid_words(correct_index_letters_try)
+        final_list = self._final_list(valid_words, invalid_words)
+        new_try = self._word_from_list(final_list)
         return new_try
 
     def get_attempt(self):
+        """Return a new attempt."""
         if self.num_attempts == 0:
-            return self.first_attempt()
+            return self._first_attempt()
         else:
-            return self.new_attempt()
-    
-    def update_data(self,word):
+            return self._new_attempt()
+
+    def _update_data(self, word):
         self._increase_num_attempts()
         self._set_last_attempt(word)
         self._append_attempt()
-        
 
-    def send_attemp(self,word):
-        self.update_data(word)
-        #result = requests.post(self.results_endpoint,json = {"result_word": word}, auth = self.user)
-        result = {'word_sent': 'abiertos', 'score': 0.25, 'try_datetime': '2022-06-23T22:12:03.226984', 'position_array': [True, False, False, False, False, False, False, False], 'right_letters_in_wrong_positions': ['t', 'o'], 'current_attemps': 1}
-        
+    def send_attemp(self, word):
+        self._update_data(word)
+        result = requests.post(
+            self.results_endpoint, json={"result_word": word}, auth=self.user
+        )
+        print("WTF", result.json())
+        self._append_response(result.json())
         self._append_result(result)
-        self.last_result_json = result  #ponerle el .json
+        self.__last_result_json = result.json()
         return result
 
-    def send_attemp1(self,word):
-        self.update_data(word)
-        #result = requests.post(self.results_endpoint,json = {"result_word": word}, auth = self.user)
-        result = {'word_sent': 'actuando', 'score': 0.375, 'try_datetime': '2022-06-23T22:12:12.449979', 'position_array': [True, False, False, False, False, False, True, True], 'right_letters_in_wrong_positions': ['t', 'a'], 'current_attemps': 2}
-        self._append_result(result)
-        self.last_result_json = result  #ponerle el .json
-        return result
+    def _security(self):
+        x = input("Write jorge for stop:")
+        if x == "jorge":
+            return 1
+        else:
+            return self.__last_result_json["score"]
 
-    def send_attemp2(self,word):
-        self.update_data(word)
-        #result = requests.post(self.results_endpoint,json = {"result_word": word}, auth = self.user)
-        result = {'word_sent': 'adaptado', 'score': 0.875, 'try_datetime': '2022-06-23T22:15:02.986128', 'position_array': [True, True, False, True, True, True, True, True], 'right_letters_in_wrong_positions': [], 'current_attemps': 3}
-        self._append_result(result)
-        self.last_result_json = result  #ponerle el .json
-        return result
-    
     def play(self):
-        #sending first attemp to the endpoint
-        self.first_attempt()
-        self.send_attemp(self.first_attempt())
-        # self.send_attemp1(self.new_attempt())
-        print(self.new_attempt())
-        
+        score = 0
+        while score != 1:
+            self.send_attemp(self.get_attempt())
+            score = self._security()
 
+    def play_automatic(self):
+        score = 0
+        while score != 1:
+            self.send_attemp(self.get_attempt())
+            score = self.__last_result_json["score"]
 
- 
+    def _append_response(self, response):
+        """Add the responses to the json file """
+        listObj = []
+        if path.isfile(self.filename) is False:
+            raise Exception("File not found")
+        with open(self.filename) as fp:
+            listObj = json.load(fp)
+        listObj.append(response)
+        with open(self.filename, "w") as json_file:
+            json.dump(listObj, json_file, indent=4, separators=(",", ": "))
 
-    
+    @classmethod
+    def responses_json(cls):
+        """Return all responses stored in the json file"""
+        if path.isfile(cls.filename) is False:
+            raise Exception("File not found")
+        with open(cls.filename) as fp:
+            list_responses = json.load(fp)
+        return list_responses
+
+    @classmethod
+    def send_try(cls, word):
+        result = requests.post(
+            cls.results_endpoint,
+            json={"result_word": word},
+            auth=cls.default_user,
+        )
+        return result
+
+    @classmethod
+    def append_response(cls, response):
+        """Return all responses stored in the json file"""
+        listObj = []
+        if path.isfile(cls.filename) is False:
+            raise Exception("File not found")
+        with open(cls.filename) as fp:
+            listObj = json.load(fp)
+        listObj.append(response)
+        with open(cls.filename, "w") as json_file:
+            json.dump(listObj, json_file, indent=4, separators=(",", ": "))
